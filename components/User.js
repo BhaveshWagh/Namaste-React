5a64eabb2575490367ee72afb9704288d4118fa6
BhaveshWagh <bhaveshbm2711@gmail.com>
2023-12-28 17:12:16 +0530

    React Life-Cycle Method

--- components/User.js
+++ components/User.js
@@ -1,8 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const User = ({ name }) => {
-  const [count,setCount] = useState(0);
+  const [count, setCount] = useState(0);
   const [count2] = useState(1);
+
+  useEffect(() => {
+    // API Call
+
+    // When we render this the render is log then useEffect and when the when the useEffect return will loged after we leave a page 
+    const timer = setInterval(() => {
+      console.log("NAMASTE REACT OP");
+    }, 1000);
+
+    console.log("useEffect")
+    
+    return () => {
+      clearInterval(timer)
+      console.log("UseEFFect return ") // after leaving the page it will printed
+    }
+  }, []);
+
+  console.log("render")
+
   return (
     <div className="user-card">
       <h1>
